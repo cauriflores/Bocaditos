b76cdbf2561d7106f87d33bf1a6e0a80800f1585
Cauri Flores <cauriflores@gmail.com>
2023-11-14 19:11:12 +0100

    Initial Commit

---
+++ Bocaditos/BocaditosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BocaditosApp.swift
+//  Bocaditos
+//
+//  Created by CAURI on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BocaditosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
